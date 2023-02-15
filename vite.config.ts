08193261b90d21e375f3bcdb790ec5447b1d3f01
slm <sunliangmu@sensordata.cn>
2023-02-15 11:40:35 +0800

    fix: 优化代码

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { resolve } from 'path';
+// import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import { version } from './package.json';
@@ -7,12 +7,12 @@ export default defineConfig({
   plugins: [],
   // 打包配置
   build: {
-    lib: {
-      formats: ['umd'],
-      entry: resolve(__dirname, 'src/index.ts'), // 入口
-      name: '__WEB_AUDIO__', // 安裝、引入用
-      fileName: (format: string) => `web-audio.${format}.[hash].js`, // 打包后名称
-    },
+    // lib: {
+    //   formats: ['umd'],
+    //   entry: resolve(__dirname, 'src/index.ts'), // 入口
+    //   name: '__WEB_AUDIO__', // 安裝、引入用
+    //   fileName: (format: string) => `web-audio.${format}.[hash].js`, // 打包后名称
+    // },
     emptyOutDir: true,
     outDir: 'lib',
     sourcemap: true, // 输出sourcemap
