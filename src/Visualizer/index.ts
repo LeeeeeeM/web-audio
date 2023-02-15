37de3ceed7cec10b7af0b9166b5e920bb991ce59
slm <sunliangmu@sensordata.cn>
2023-02-15 13:46:58 +0800

    feat: 添加频谱

--- src/Visualizer/index.ts
+++ src/Visualizer/index.ts
@@ -1,3 +1,9 @@
+enum DOMAIN {
+    TIME = 'time',
+    FREQUENCY = 'fre',
+}
+
+
 export default class Visualizer {
   private audioContext: AudioContext;
 //   private canvas: HTMLCanvasElement;
@@ -8,6 +14,7 @@ export default class Visualizer {
   private paused: boolean;
   private frame: number;
   private frameData: Uint8Array;
+  private domain: DOMAIN;
 //   private gainNode: GainNode;
   constructor(audioContext: AudioContext, canvas: HTMLCanvasElement) {
     this.audioContext = audioContext;
@@ -22,14 +29,27 @@ export default class Visualizer {
     this.analyser.smoothingTimeConstant = 0.85;
     this.analyser.fftSize = Math.pow(2, 11);
 
+    this.domain = DOMAIN.TIME;
+
     this.paused = false;
     this.frame = 0;
     this.frameData = new Uint8Array(this.analyser.fftSize);
   }
 
+  pause(source: AudioBufferSourceNode) {
+    this.paused = true;
+    cancelAnimationFrame(this.frame);
+    this.disconnect(source);
+  }
+  start(source: AudioBufferSourceNode) {
+    this.paused = false;
+    this.draw();
+    this.connect(source);
+  }
+
   connect(source: AudioBufferSourceNode) {
     const audioContext = this.audioContext;
-    console.log(audioContext);
+
     // const distortion = audioContext.createWaveShaper();
     const gainNode = audioContext.createGain();
     gainNode.gain.value = 0.2;
@@ -55,25 +75,55 @@ export default class Visualizer {
 
   draw() {
     if (!this.paused) {
-      this.analyser.getByteTimeDomainData(this.frameData);
-    //   console.log(this.frameData);
-      this.render(this.canvasContext, this.frameData, this.analyser.fftSize);
+        if (this.domain === DOMAIN.TIME) {
+            this.drawWave();
+        } else {
+            this.drawFre();
+        }
     }
     this.frame = requestAnimationFrame(() => {
       this.draw();
     });
   }
-  pause(source: AudioBufferSourceNode) {
-    this.paused = true;
-    cancelAnimationFrame(this.frame);
-    this.disconnect(source);
+
+  drawWave() {
+    this.analyser.fftSize = Math.pow(2, 11);
+    this.analyser.getByteTimeDomainData(this.frameData);
+    this.renderWave(this.canvasContext, this.frameData, this.analyser.fftSize);
   }
-  start(source: AudioBufferSourceNode) {
-    this.paused = false;
-    this.draw();
-    this.connect(source);
+
+  drawFre() {
+    this.analyser.fftSize = Math.pow(2, 8);
+    // 奈奎斯特定律
+    this.frameData = new Uint8Array(this.analyser.frequencyBinCount);
+    this.analyser.getByteFrequencyData(this.frameData);
+    this.renderFre(this.canvasContext, this.frameData, this.analyser.frequencyBinCount);
   }
-  render(context: CanvasRenderingContext2D, data: Uint8Array, len: number) {
+
+  renderFre(context: CanvasRenderingContext2D, data: Uint8Array, len: number) {
+    context.fillStyle = "rgb(0, 0, 0)";
+    context.fillRect(0, 0, this.width, this.height);
+
+    const barWidth = (this.width / len) * 2.5;
+    let barHeight;
+    let x = 0;
+
+    for (let i = 0; i < len; i++) {
+      barHeight = data[i];
+
+      context.fillStyle = "rgb(50,50," + (barHeight + 100) + ")";
+      context.fillRect(
+        x,
+        this.height - barHeight / 2,
+        barWidth,
+        barHeight / 2
+      );
+
+      x += barWidth + 1;
+    }
+  }
+
+  renderWave(context: CanvasRenderingContext2D, data: Uint8Array, len: number) {
     context.fillStyle = "rgb(200, 200, 200)";
     context.fillRect(0, 0, this.width, this.height);
 
