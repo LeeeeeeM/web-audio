1d603f70da17d7ac7380d987a5a38ccff2d2d407
slm <sunliangmu@sensordata.cn>
2023-02-16 11:26:58 +0800

    feat: 收敛代码

--- src/Visualizer/index.ts
+++ src/Visualizer/index.ts
@@ -1,25 +1,30 @@
-enum DOMAIN {
-    TIME = 'time',
-    FREQUENCY = 'fre',
+enum DOMAIN_ENUM {
+  TIME = 'time',
+  FREQUENCY = 'fre',
 }
-
 
 export default class Visualizer {
   private audioContext: AudioContext;
-//   private canvas: HTMLCanvasElement;
   private canvasContext: CanvasRenderingContext2D;
+  private source: AudioBufferSourceNode;
   private width: number;
   private height: number;
   private analyser: AnalyserNode;
   private paused: boolean;
   private frame: number;
   private frameData: Uint8Array;
-  private domain: DOMAIN;
-//   private gainNode: GainNode;
-  constructor(audioContext: AudioContext, canvas: HTMLCanvasElement) {
+  private domain: DOMAIN_ENUM;
+
+  // 设置下次继续的位置
+  private offset: number;
+  // 记录距离开始时的偏移量
+  private startOffset: number;
+
+  //   private gainNode: GainNode;
+  constructor(audioContext: AudioContext, canvas: HTMLCanvasElement, source: AudioBufferSourceNode) {
     this.audioContext = audioContext;
-    // this.canvas = canvas;
     this.canvasContext = canvas.getContext('2d')!;
+    this.source = source;
     this.width = canvas.width;
     this.height = canvas.height;
 
@@ -29,57 +34,100 @@ export default class Visualizer {
     this.analyser.smoothingTimeConstant = 0.85;
     this.analyser.fftSize = Math.pow(2, 11);
 
-    this.domain = DOMAIN.TIME;
+    this.domain = DOMAIN_ENUM.TIME;
 
     this.paused = false;
     this.frame = 0;
     this.frameData = new Uint8Array(this.analyser.fftSize);
+
+    this.offset = 0;
+    this.startOffset = 0;
   }
 
-  pause(source: AudioBufferSourceNode) {
+  pause() {
     this.paused = true;
     cancelAnimationFrame(this.frame);
-    this.disconnect(source);
+    this.disconnect();
+    this.source.stop(0);
+    // 设置暂停时的偏移量
+    this.offset = this.offset + this.audioContext.currentTime - this.startOffset;
   }
-  start(source: AudioBufferSourceNode) {
+
+  start() {
     this.paused = false;
     this.draw();
-    this.connect(source);
+    this.connect();
+    this.startOffset = this.audioContext.currentTime;
+    this.source.start(0, this.offset);
   }
 
-  connect(source: AudioBufferSourceNode) {
+  stop() {
+    this.paused = true;
+    cancelAnimationFrame(this.frame);
+    this.clearVisual();
+    this.disconnect();
+    this.source.stop(0);
+    // 清空偏移量
+    this.offset = 0;
+  }
+
+  clearVisual() {
+    const context = this.canvasContext;
+    context.fillStyle = 'rgb(200, 200, 200)';
+    context.fillRect(0, 0, this.width, this.height);
+  }
+
+  changeFreDomain() {
+    this.domain = DOMAIN_ENUM.FREQUENCY;
+    this.analyser.fftSize = Math.pow(2, 8);
+    // 奈奎斯特定律
+    this.frameData = new Uint8Array(this.analyser.frequencyBinCount);
+  }
+
+  changeTimeDomain() {
+    this.domain = DOMAIN_ENUM.TIME;
+    this.analyser.fftSize = Math.pow(2, 11);
+    this.frameData = new Uint8Array(this.analyser.fftSize);
+  }
+
+  connect() {
     const audioContext = this.audioContext;
 
-    // const distortion = audioContext.createWaveShaper();
+    const copyBuffer = this.source.buffer;
+    const newSource = audioContext.createBufferSource();
+    newSource.buffer = copyBuffer;
+    this.source = newSource;
+
     const gainNode = audioContext.createGain();
     gainNode.gain.value = 0.2;
+
+    // const distortion = audioContext.createWaveShaper();
     // const biquadFilter = audioContext.createBiquadFilter();
     // const convolver = audioContext.createConvolver();
-  
     // const echoDelay = createEchoDelayEffect(audioCtx);
-
-    source.connect(this.analyser);
     // distortion.connect(biquadFilter);
     // biquadFilter.connect(gainNode);
     // convolver.connect(gainNode);
     // echoDelay.placeBetween(gainNode, analyser);
+
+    this.source.connect(this.analyser);
     this.analyser.connect(gainNode);
     gainNode.connect(audioContext.destination);
   }
 
-  disconnect(source: AudioBufferSourceNode) {
-    source.disconnect();
+  disconnect() {
+    this.source.disconnect();
     // 必须断开analyser，否则后续链接的gainNode也会继续放大声音
     this.analyser.disconnect();
   }
 
   draw() {
     if (!this.paused) {
-        if (this.domain === DOMAIN.TIME) {
-            this.drawWave();
-        } else {
-            this.drawFre();
-        }
+      if (this.domain === DOMAIN_ENUM.TIME) {
+        this.drawWave();
+      } else {
+        this.drawFre();
+      }
     }
     this.frame = requestAnimationFrame(() => {
       this.draw();
@@ -87,21 +135,17 @@ export default class Visualizer {
   }
 
   drawWave() {
-    this.analyser.fftSize = Math.pow(2, 11);
     this.analyser.getByteTimeDomainData(this.frameData);
     this.renderWave(this.canvasContext, this.frameData, this.analyser.fftSize);
   }
 
   drawFre() {
-    this.analyser.fftSize = Math.pow(2, 8);
-    // 奈奎斯特定律
-    this.frameData = new Uint8Array(this.analyser.frequencyBinCount);
     this.analyser.getByteFrequencyData(this.frameData);
     this.renderFre(this.canvasContext, this.frameData, this.analyser.frequencyBinCount);
   }
 
   renderFre(context: CanvasRenderingContext2D, data: Uint8Array, len: number) {
-    context.fillStyle = "rgb(0, 0, 0)";
+    context.fillStyle = 'rgb(0, 0, 0)';
     context.fillRect(0, 0, this.width, this.height);
 
     const barWidth = (this.width / len) * 2.5;
@@ -111,24 +155,19 @@ export default class Visualizer {
     for (let i = 0; i < len; i++) {
       barHeight = data[i];
 
-      context.fillStyle = "rgb(50,50," + (barHeight + 100) + ")";
-      context.fillRect(
-        x,
-        this.height - barHeight / 2,
-        barWidth,
-        barHeight / 2
-      );
+      context.fillStyle = 'rgb(50,50,' + (barHeight + 100) + ')';
+      context.fillRect(x, this.height - barHeight / 2, barWidth, barHeight / 2);
 
       x += barWidth + 1;
     }
   }
 
   renderWave(context: CanvasRenderingContext2D, data: Uint8Array, len: number) {
-    context.fillStyle = "rgb(200, 200, 200)";
+    context.fillStyle = 'rgb(200, 200, 200)';
     context.fillRect(0, 0, this.width, this.height);
 
     context.lineWidth = 2;
-    context.strokeStyle = "rgb(0, 0, 0)";
+    context.strokeStyle = 'rgb(0, 0, 0)';
 
     context.beginPath();
 
--- src/index.ts
+++ src/index.ts
@@ -6,78 +6,92 @@ const audioCtx = new window.AudioContext();
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
-const visualizer = new Visualizer(audioCtx, canvas);
-
-let source: AudioBufferSourceNode;
-
-let sourceBuffer: AudioBuffer | null = null;
-
-let offset = 0;
-let startOffset = 0;
-
-const getMusiceBuffer = async (url = 'https://scms-test-cdaa.obs.cn-east-3.myhuaweicloud.com/music_web/jiumengyichang.mp3') => {
-    if (sourceBuffer) {
-        return sourceBuffer;
-    }
-    return request({
-        method: 'GET',
-        url,
-        responseType: 'arraybuffer'
-    }).then(({ data }) => {
-        return audioCtx.decodeAudioData(data);
-    }).then(buffer => {
-        sourceBuffer = buffer;
-        return buffer;
-    });
-}
-
-
-const fetchMusicSource = async () => {
-    source = audioCtx.createBufferSource();
-
-    const sourceBuffer = await getMusiceBuffer();
-    source.buffer = sourceBuffer;
-
-    return source;
-}
+const time = document.getElementById('time') as HTMLInputElement;
+const frequency = document.getElementById('frequency') as HTMLInputElement;
+time.disabled = true;
+frequency.disabled = true;
 
 const startBtn = document.getElementById('start') as HTMLButtonElement;
 const stopBtn = document.getElementById('stop') as HTMLButtonElement;
 const pauseBtn = document.getElementById('pause') as HTMLButtonElement;
 
-startBtn.disabled = false;
+startBtn.disabled = true;
 stopBtn.disabled = true;
 pauseBtn.disabled = true;
 
-startBtn?.addEventListener('click', async () => {
+let source: AudioBufferSourceNode;
+
+let sourceBuffer: AudioBuffer | null = null;
+
+const getBuffer = async (
+  url = 'https://scms-test-cdaa.obs.cn-east-3.myhuaweicloud.com/music_web/jiumengyichang.mp3'
+) => {
+  if (sourceBuffer) {
+    return sourceBuffer;
+  }
+  return request({
+    method: 'GET',
+    url,
+    responseType: 'arraybuffer',
+  })
+    .then(({ data }) => {
+      return audioCtx.decodeAudioData(data);
+    })
+    .then(buffer => {
+      sourceBuffer = buffer;
+      return buffer;
+    });
+};
+
+const composeSource = async () => {
+  source = audioCtx.createBufferSource();
+  const sourceBuffer = await getBuffer();
+  source.buffer = sourceBuffer;
+  return source;
+};
+
+const main = async () => {
+  source = await composeSource();
+
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  pauseBtn.disabled = true;
+
+  time.disabled = false;
+  frequency.disabled = false;
+
+  const visualizer = new Visualizer(audioCtx, canvas, source);
+
+  time?.addEventListener('change', (e) => {
+    visualizer.changeTimeDomain();
+  });
+
+  frequency?.addEventListener('change', (e) => {
+    visualizer.changeFreDomain();
+  });
+
+  startBtn?.addEventListener('click', () => {
     startBtn.disabled = true;
     stopBtn.disabled = false;
     pauseBtn.disabled = false;
-    const source = await fetchMusicSource();
-    visualizer.start(source);
-    // console.log(audioCtx.currentTime);
-    startOffset = audioCtx.currentTime;
-    source.start(0, offset);
-});
-
-pauseBtn?.addEventListener('click', () => {
+    visualizer.start();
+  });
+  
+  pauseBtn?.addEventListener('click', () => {
     startBtn.disabled = false;
     stopBtn.disabled = false;
     pauseBtn.disabled = true;
-    visualizer.pause(source);
-    source.stop(0);
-    // 设置下次继续的位置
-    offset = offset + audioCtx.currentTime - startOffset;
-});
-
-stopBtn?.addEventListener('click', () => {
+    visualizer.pause();
+  });
+  
+  stopBtn?.addEventListener('click', () => {
     startBtn.disabled = false;
     stopBtn.disabled = true;
     pauseBtn.disabled = true;
-    visualizer.pause(source);
-    source.stop(0);
-    // 设置下次继续的位置
-    offset = 0; 
-});
+    visualizer.stop();
+  });
+};
+
+main();
 
 
